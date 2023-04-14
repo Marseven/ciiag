89ec72588066960a23e68bf35d154755f95dba34
Aristide Mebodo <mebodoaristide@yahoo.fr>
2023-04-14 12:55:13 +0100

    DD1

--- resources/views/admin/registration/index.blade.php
+++ resources/views/admin/registration/index.blade.php
@@ -139,7 +139,7 @@
 
     <script>
         $(document).ready(function() {
-            $('#datatable_buttons').DataTable({
+            $('#datatable-buttons').DataTable({
                 language: {
                     url: "https://cdn.datatables.net/plug-ins/1.10.25/i18n/French.json"
                 },
