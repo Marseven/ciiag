b67e1bce4519b3716dfa5be3506c61965f5bfa99
Aristide Mebodo <mebodoaristide@yahoo.fr>
2023-04-20 11:52:56 +0100

    Debug

--- resources/views/layouts/flash.blade.php
+++ resources/views/layouts/flash.blade.php
@@ -1,34 +1,28 @@
 @if ($message = Session::get('success'))
-
     <div class="alert alert-success " role="alert">
         {{ $message }}
     </div>
 @endif
 
 @if ($message = Session::get('error'))
-
     <div class="alert alert-danger " role="alert">
         {{ $message }}
     </div>
 @endif
 
 @if ($message = Session::get('warning'))
-
     <div class="alert alert-warning " role="alert">
         {{ $message }}
     </div>
 @endif
 
 @if ($message = Session::get('info'))
-
     <div class="alert alert-info " role="alert">
         {{ $message }}
     </div>
 @endif
 
 @if ($errors->any())
-
-
     <div class="alert alert-danger " role="alert">
         @foreach ($errors->all() as $error)
             <div class="alert-text">{{ $error }}</div>
